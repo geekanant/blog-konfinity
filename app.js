d889a80b7ceb54473b749fe672c504a8bfbe6421
geekanant <anantpatni1@gmail.com>
2019-06-10 11:32:10 -0400

    Setup express server

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.set("port", 8888); 
+
+app.listen(app.get("port"), () => {
+  console.log("Application running in port: " + app.get("port"));
+});
+
+module.exports = app;
+
+
